38861b230565acdf4d91ec7708432ec526bf6e22
Richard madrigar <richard_madrigar99@hotmail.com>
2021-08-24 18:20:44 -0300

    att

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-function showTime(){
-  let date = new Date();
+function showTime() {
+  var date = new Date(); //pegando a data compvara
 
-  let hours = date.getHours()
-  let minutes = date.getMinutes()
-  let seconds = date.getSeconds()
+  var hours = date.getHours() //pegando horas
+  var minutes = date.getMinutes() //pegando minutos
+  var seconds = date.getSeconds() //pegando segundos
 
-  if(hours == 0) {
+  //transformando 0 em 12hrs
+  if (hours == 0) {
     hours = 12;
   }
 
 
-
-  hours = (hours<10) ? "0" + hours : hours
-  minutes = (minutes<10) ? "0" + minutes : minutes
-  seconds = (seconds<10) ? "0" + seconds : seconds
+  hours = (hours < 10) ? "0" + hours : hours
+  minutes = (minutes < 10) ? "0" + minutes : minutes
+  seconds = (seconds < 10) ? "0" + seconds : seconds
 
   let time = `${hours}:${minutes}:${seconds} `
   document.getElementById('clockDisplay').textContent = time
